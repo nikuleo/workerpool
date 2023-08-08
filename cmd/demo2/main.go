3ea9382f3538c3e61dd5f4076e969932edcb0f24
niku <nikuliu@foxmail.com>
2023-08-08 16:02:30 +0800

    🦄 refactor: 修改 demo 案例

--- cmd/demo2/main.go
+++ cmd/demo2/main.go
@@ -2,22 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 	workerpool "workerpool/pool"
 )
 
+func demoFunc() {
+	time.Sleep(time.Second * 3)
+}
+
 func main() {
-	p := workerpool.New(5, workerpool.WithPreAllocWorkers(false), workerpool.WithBlock(false))
+	p := workerpool.New(5, workerpool.WithPreAllocWorkers(false), workerpool.WithBlock(true))
+	defer p.Free()
+
+	var wg sync.WaitGroup
 
 	time.Sleep(time.Second * 3)
-	for i := 0; i < 10; i++ {
-		err := p.Schedule(func() {
-			time.Sleep(time.Second * 3)
-		})
+	for i := 0; i < 15; i++ {
+		wg.Add(1)
+		task := func() {
+			demoFunc()
+			wg.Done()
+		}
+		err := p.Schedule(task)
 		if err != nil {
 			fmt.Printf("task[%d]: error: %s\n", i, err.Error())
 		}
 	}
 
-	p.Free()
 }
